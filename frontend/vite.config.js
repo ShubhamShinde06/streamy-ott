05c78cbd59677944ef1bf1a6216ceedf6edca397
ShubhamShinde06 <168455735+ShubhamShinde06@users.noreply.github.com>
2025-02-10 06:58:02 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,16 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  server: {
-    host: 'localhost',
-    port: 5173,
-    hmr: {
-      protocol: 'ws',
-      host: 'localhost',
-      port: 5173,
-    },
+  server:{
     proxy:{
-      '/api':'https://streamy-ott-backend.onrender.com'
+      '/api':'https://streamy-ott-backend.onrender.com/'
     }
   },
   plugins: [react()],
